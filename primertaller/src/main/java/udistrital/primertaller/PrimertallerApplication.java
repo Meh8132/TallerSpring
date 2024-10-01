bcb4d5f0c1025e2178bc77a22689a1e69e175f12
Juan Camilo Herrera <juan8132juan@gmail.com>
2024-10-01 17:40:06 -0500

    Primer commit

---
+++ primertaller/src/main/java/udistrital/primertaller/PrimertallerApplication.java
@@ -0,0 +1,13 @@
+package udistrital.primertaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimertallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimertallerApplication.class, args);
+	}
+
+}
---
+++ primertaller/src/test/java/udistrital/primertaller/PrimertallerApplicationTests.java
@@ -0,0 +1,13 @@
+package udistrital.primertaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimertallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
